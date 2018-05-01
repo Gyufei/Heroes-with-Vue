8c465fc533dd431e3063077f5149e4a602e14405
afei <guo812773673@163.com>
2018-05-01 22:22:10 +0800

    add heroes component deletehero method

--- src/heroes/heroes.vue
+++ src/heroes/heroes.vue
@@ -10,7 +10,10 @@
         </div>
 
         <ul>
-            <li v-for="hero of heroes" :key="hero.id">{{hero.name}}</li>
+            <li v-for="hero of heroes" :key="hero.id">
+                <span>{{hero.id}}</span>{{hero.name}}
+                <button @click="deleteHero(hero)">X</button>
+            </li>
         </ul>
     </div>
 </template>
@@ -28,13 +31,16 @@
             }
         },
         methods:{
-            //此处后续需要使用 vuex 重写，此时仅仅象征性的添加
+            //此处后续需要使用 vuex 重写，此时仅仅象征性的添加一下^_^
             addHero:function(){
                 let newId = this.heroes[this.heroes.length-1]['id'];
                 let newHero = { id: ++newId, name: this.heroName };
                 this.heroes.push(newHero);
                 this.heroName = '';
                 this.$refs.nameInput.focus();
+            },
+            deleteHero:function(hero){
+                this.heroes = this.heroes.filter((h)=>hero != h)
             }
         }
     }
